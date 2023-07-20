145d7eeb1c7b1fd95740f818f59f1a4b0d2cb33e
Victor <victor.burton@hotmail.fr>
2023-07-20 14:37:44 +0200

    add protection for unharmed HumanB

--- CPP1/ex03/HumanB.cpp
+++ CPP1/ex03/HumanB.cpp
@@ -6,18 +6,23 @@
 /*   By: vburton <vburton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 18:07:37 by vburton           #+#    #+#             */
-/*   Updated: 2023/07/19 18:20:07 by vburton          ###   ########.fr       */
+/*   Updated: 2023/07/20 14:14:51 by vburton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(const std::string &name): _name(name){}
+HumanB::HumanB(const std::string &name): _name(name){
+	_weapon = NULL;
+}
 
 HumanB::~HumanB(){}
 
 void	HumanB::attack(void){
-	std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
+	if (!this->_weapon)
+		std::cout << _name << " don't have any weapon to attack." << std::endl;
+	else
+		std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
 }
 
 void	HumanB::setWeapon(Weapon &newWeapon){
