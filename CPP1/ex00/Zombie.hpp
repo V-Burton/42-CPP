cda1c5ca60399fe540b4a18ca83f46fa178a4362
Victor <victor.burton@hotmail.fr>
2023-07-20 18:58:36 +0200

    correct missclick

--- CPP1/ex00/Zombie.hpp
+++ CPP1/ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: vburton <vburton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 09:13:09 by vburton           #+#    #+#             */
-/*   Updated: 2023/07/19 14:17:39 by vburton          ###   ########.fr       */
+/*   Updated: 2023/07/20 17:41:11 by vburton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ class Zombie
     public:
         Zombie(const std::string &name);
         ~Zombie(void);
-        void    announce(void) const;zz
+        void    announce(void) const;
 
     private:
         std::string _name;
