d68e241629dc2498812e56a72292013eedf6bf04
Victor <victor.burton@hotmail.fr>
2023-07-20 16:29:11 +0200

    moove print info from main to harl.cpp

--- CPP1/ex05/Harl.cpp
+++ CPP1/ex05/Harl.cpp
@@ -15,18 +15,22 @@ Harl::Harl(){
 Harl::~Harl(){}
 
 void    Harl::debug(void){
+    std::cout << UNDERLINE << BOLD << "[ DEBUG ]" << END << std::endl;
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void    Harl::info(void){
+    std::cout << UNDERLINE << BOLD << "[ INFO ]" << END << std::endl;
     std::cout << "I cannot believe adding extra bacon costs more money. You didn’t putenough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void    Harl::warning(void){
-        std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << UNDERLINE << BOLD << "[ WARNING ]" << END << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void    Harl::error(void){
+    std::cout << UNDERLINE << BOLD << "[ ERROR ]" << END << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
--- CPP1/ex05/main.cpp
+++ CPP1/ex05/main.cpp
@@ -3,25 +3,16 @@
 int main(void){
     Harl    harl;
 
-    std::cout << UNDERLINE << BOLD << "Debug:" << END << std::endl;
-    std::cout << "\t";
     harl.complain("DEBUG");
     std::cout << std::endl;
-    std::cout << UNDERLINE << BOLD << "Info:" << END << std::endl;
-    std::cout << "\t";
     harl.complain("INFO");
     std::cout << std::endl;
-    std::cout << UNDERLINE << BOLD << "Warning:" << END << std::endl;
-    std::cout << "\t";
     harl.complain("WARNING");
     std::cout << std::endl;
-    std::cout << UNDERLINE << BOLD << "Error:" << END << std::endl;
-    std::cout << "\t";
     harl.complain("ERROR");
     std::cout << std::endl;
-    std::cout << UNDERLINE << BOLD << "No level:" << END << std::endl;
-    std::cout << "\t";
-    harl.complain("No Level");
+    std::cout << UNDERLINE << BOLD << "[ WRONG ]" << END << std::endl;
+    harl.complain("WRONG");
     std::cout << std::endl;
     return (0);
 }
