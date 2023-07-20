5a08ae47b7b3ff834a991b31a0dd230836145794
Victor <victor.burton@hotmail.fr>
2023-07-20 17:29:55 +0200

    simplify cast upper

--- CPP0/ex00/megaphone.cpp
+++ CPP0/ex00/megaphone.cpp
@@ -6,32 +6,28 @@
 /*   By: vburton <vburton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 11:19:26 by vburton           #+#    #+#             */
-/*   Updated: 2023/07/17 11:19:27 by vburton          ###   ########.fr       */
+/*   Updated: 2023/07/20 17:28:32 by vburton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-using std::cout;
-using std::endl;
-using std::string;
-
 int main(int argc, char **argv)
 {
-	string	str;
+	std::string	str;
 
 	if (argc == 1)
 	{
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (1);
 	}
 	for (int i = 1; i < argc; i++)
 	{
 		str = argv[i];
 		for (size_t j = 0; j < str.length(); j++)
-			cout << static_cast<char>(toupper(str[j])); 
+			std::cout << (char)(toupper(str[j])); 
 	}
-	cout << endl;
+	std::cout << std::endl;
 	return (0);
 }
