776ee45b4aa11307773bfc24de8a10715d73ef11
Victor <victor.burton@hotmail.fr>
2023-09-04 15:12:14 +0200

    add const type

--- CPP8/ex02/MutantStack.hpp
+++ CPP8/ex02/MutantStack.hpp
@@ -21,5 +21,7 @@ template<typename T> class MutantStack: public std::stack<T>{
 		typedef typename std::stack<T>::container_type::iterator iterator;
 		iterator begin(){ return (this->c.begin());};
 		iterator end(){ return (this->c.end());};
-
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		const_iterator begin() const { return (this->c.begin());};
+		const_iterator end() const { return (this->c.end());};
 };
