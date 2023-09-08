8b903478cb20c7f78cae56a2fd31102e303e780c
Victor <victor.burton@hotmail.fr>
2023-09-08 15:51:51 +0200

    work in progress: very beginning

---
+++ CPP9/ex02/PmergeMe.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+class PmergeMe{
+	public:
+		PmergeMe();
+		PmergeMe(const PmergeMe &rhs);
+		PmergeMe &operato=(const PmergeMe &rhs);
+		~PmergeMe();
+
+	private:
+
+};
